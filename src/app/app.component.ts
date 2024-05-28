a719785624865314e301a891886d8f474f1636c6
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-05-28 10:50:10 -0300

    feat: add menu and fix route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from './shared/services/storage.service';
+import { LoadingController } from '@ionic/angular';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private router: Router,
+    private storage: StorageService,
+    private loadingCtrl: LoadingController,
+    private authService: AuthService
+  ) {}
+
+  async onLogout() {
+    this.loadingCtrl.create({
+      message: 'Saindo...'
+    })
+      .then(async loadingEl => {
+        loadingEl.present();
+        this.authService.setIsAuthenticated(false);
+        await this.storage.delete('token');
+        await this.storage.delete('userId');
+        await this.storage.delete('name');
+        await this.storage.delete('role');
+        await this.storage.delete('email');
+        this.router.navigate(['/auth']);
+        loadingEl.dismiss();
+      })
+  }
 }
